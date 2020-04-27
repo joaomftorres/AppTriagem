de45570ebef03103c5a40b6e93d7365bfa6c06c9
joaomftorres <joaomftorres@gmail.com>
2020-04-27 18:34:21 +0100

    Novo logo para a app Novas personalizações estéticas

---
+++ app/src/main/java/pt/ipg/apptriagem/ActivityPacientes.java
@@ -0,0 +1,14 @@
+package pt.ipg.apptriagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPacientes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pacientes);
+    }
+}
--- app/src/main/java/pt/ipg/apptriagem/MainActivity.java
+++ app/src/main/java/pt/ipg/apptriagem/MainActivity.java
@@ -2,7 +2,11 @@ package pt.ipg.apptriagem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // btnskin = (Button) findViewById(R.id.btnskin)
     }
+
 }
