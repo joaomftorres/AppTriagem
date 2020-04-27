4f6d229c2cf3ea35d605cf0fd42902194c67e284
joaomftorres <joaomftorres@gmail.com>
2020-04-27 22:01:26 +0100

    Criação de nova activity e internacionalização da app

---
+++ app/src/main/java/pt/ipg/apptriagem/ActivityNovaTriagem.java
@@ -0,0 +1,14 @@
+package pt.ipg.apptriagem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityNovaTriagem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_triagem);
+    }
+}
--- app/src/main/java/pt/ipg/apptriagem/ActivityTriagens.java
+++ app/src/main/java/pt/ipg/apptriagem/ActivityTriagens.java
@@ -2,7 +2,9 @@ package pt.ipg.apptriagem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityTriagens extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ActivityTriagens extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triagens);
+    }
+
+    public void buttonnovatriagem(View view) {
+
+        Intent intentnovatriagem = new Intent(this, ActivityNovaTriagem.class);
+
+        startActivity(intentnovatriagem);
     }
 }
